5d17b0166e1103e743d8d5c283389358bb0463e3
aysudeliahmetoglu <aysudeliahmetoglu@gmail.com>
2022-11-14 21:58:05 +0300

    cocukkitap,dünyadilleri,kategoriler,oyunlar design

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,6 +7,12 @@
       <ayınyayınevi />
       <!-- <ayınyazarları /> -->
       <ilgigörenler />
+      <kategoriler/>
+      <cocukKitaplari />
+      <oyundan_dahafazlası />
+      <dünya_dilleri/>
+
+
       
 
       
@@ -19,12 +25,19 @@
   import ayınyayınevi from '../components/ayınyayınevi.vue';
   // import ayınyazarları from '../components/ayınyazarları.vue';
   import ilgigörenler from '../components/ilgigörenler.vue';
+  import kategoriler from '../components/kategoriler.vue';
+import cocukKitaplari from '../components/cocukKitaplari.vue';
+import oyundan_dahafazlası from '../components/oyundan_dahafazlası.vue';
+import dünya_dilleri from '../components/dünya_dilleri.vue';
+
+
+
 
 
   
  
   export default { 
       name: "IndexPage", 
-      components: { Navbar,Slider,haftanınyayınevi,ayınyayınevi,ilgigörenler} 
+      components: { Navbar,Slider,haftanınyayınevi,ayınyayınevi,ilgigörenler,kategoriler,cocukKitaplari,oyundan_dahafazlası,dünya_dilleri} 
   } 
   </script>
