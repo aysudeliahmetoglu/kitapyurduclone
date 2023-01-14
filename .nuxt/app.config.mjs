5b59258e458897d11e3102e9cd6742fa9734a3b9
aysudeliahmetoglu <aysudeliahmetoglu@gmail.com>
2023-01-14 21:33:13 +0300

    test

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/home/aysu/Desktop/vuenuxtkitap/kitapyurduvue/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default defuFn(inlineConfig)
---
+++ .nuxt/dist/server/server.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///home/aysu/Desktop/vuenuxtkitap/kitapyurduvue/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
